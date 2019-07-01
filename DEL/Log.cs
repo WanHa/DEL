08e26b39a626af6facd9f0bd74befd64a701768f
WanHa <1043346131@qq.com>
2019-07-01 10:08:24 +0800

    Signed-off-by: WanHa <1043346131@qq.com>

--- DEL/Log.cs
+++ DEL/Log.cs
@@ -9,7 +9,8 @@ namespace DEL
 {
     public class Log
     {
-        public void WriteLog(string path, string info)        {
+        public void WriteLog(string path, string info)
+        {
             using (var stream = new StreamWriter(System.IO.Path.Combine(path, $"Data_{DateTime.Now:yyyyMMdd}.txt"), true))
             {
                 stream.WriteLine("{0:yyyy-MM-dd HH:mm:ss}, __Info__{1}", DateTime.Now, info);
